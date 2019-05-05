0a2fd3a818853fdd08633efbe923dac328e3c0df
Tatsuji Ashitani <ashipong@gmail.com>
2019-05-05 21:33:36 +0900

    all examples are ported to c++

--- 01_static_lib/test.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-#include "hoge.h"
-
-int main(){
-    printf("hello\n");
-    printf("%d\n", add(10,20));
-}
---
+++ 03_subdirectories/test.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "hoge.hpp"
+
+using namespace std;
+
+int main(){
+    cout << add(10,20) << endl;
+}
