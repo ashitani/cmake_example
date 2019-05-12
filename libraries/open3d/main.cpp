ac9634bca6cdc1ed3e9d9257d5140019705321e4
Tatsuji Ashitani <ashipong@gmail.com>
2019-05-12 15:29:53 +0900

    added open3d c++API

---
+++ libraries/open3d/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+#include "Open3D/Open3D.h"
+
+using namespace open3d;
+using namespace std;
+
+int main()
+{
+
+  string filename = "../../data/torus.ply";
+
+  auto cloud_ptr = make_shared<geometry::PointCloud>();
+
+  if (!io::ReadPointCloud(filename, *cloud_ptr))
+  {
+    cout << "Failed to read " << filename << endl;
+    return 1;
+  }
+ 
+  visualization::DrawGeometries({cloud_ptr}, "Cloud", 640,480);
+
+}
