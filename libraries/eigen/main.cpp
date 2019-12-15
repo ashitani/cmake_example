33c73fd2618b02d7cd29b38ca6caf8abab7ef792
Tatsuji Ashitani <ashipong@gmail.com>
2019-12-15 09:07:00 +0900

    modified minimum version 3.9->3.0

--- libraries/eigen/main.cpp
+++ libraries/eigen/main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <Eigen/Dense>
+#include <vector>
 using Eigen::MatrixXd;
+using Eigen::Vector3d;
 int main()
 {
   MatrixXd m(2,2);
@@ -9,4 +11,17 @@ int main()
   m(0,1) = -1;
   m(1,1) = m(1,0) + m(0,1);
   std::cout << m << std::endl;
+
+ Vector3d v(0,0,0);
+
+ std::vector<Vector3d> vv;
+
+ for(int i=0;i<10;i++){
+    v(0)=i;
+    vv.push_back(v);
+ }
+
+    for(int i=0;i<10;i++){
+     std::cout << i << ":" << vv[i] << std::endl;
+    }
 }
